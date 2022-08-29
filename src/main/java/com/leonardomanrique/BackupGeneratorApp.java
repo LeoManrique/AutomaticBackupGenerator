dcc06c76633f6b925b2f8cc1cfbbb54ba617dfcf
LeoManrique <leonardomanrique43@gmail.com>
2022-08-29 16:25:55 +0200

    initalCommit

---
+++ src/main/java/com/leonardomanrique/BackupGeneratorApp.java
@@ -0,0 +1,13 @@
+package com.leonardomanrique;
+
+/**
+ * Hello world!
+ *
+ */
+public class BackupGeneratorApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/leonardomanrique/BackupGeneratorAppTest.java
@@ -0,0 +1,20 @@
+package com.leonardomanrique;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BackupGeneratorAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
